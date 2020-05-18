f1927187a8c943065eeffe22aac2b6408ef32f48
Emil Föger <emil.foger@gmail.com>
2020-05-18 14:58:05 +0200

    Created PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Variabeldeklaration für den gesamten Class-Scope
+    public float moveSpeed;
+    private Vector2 moveInput; // Eingabe als Vektor
+
+    public Rigidbody2D theRB; // Kollisionskörper für Spieler
+
+    public Transform gunArm;    
+    private Camera theCam;
+
+    public Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Cameraobjekt abspeichern, damit dies nicht jedes Frame im Gesampten Projekt gesucht wird
+        theCam = Camera.main;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Bewegungseingabe (W,A,S,D) registreiren, speichern und als Einheitsvektor normalisieren
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+        theRB.velocity = moveInput * moveSpeed;
+
+
+        // Mausposition als Bildschirmkoordinate speichern; Globale Spielerposition in Bildschirmkoordinaten umwandeln
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
+
+
+        // Spieler und Waffe link/rechts zur Mausposition spiegeln, mithilfe negativer Spiegelung des Sprites
+        if (mousePos.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunArm.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            gunArm.localScale = Vector3.one;
+        }
+
+
+        // Waffenrotation von Maus- und Spielerposition als Winkel ableiten und als Quaternionangabe übergeben
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+        gunArm.rotation = Quaternion.Euler(0, 0, angle);
+
+
+        // Animations-switch für Stillstand und Bewegung
+        if (moveInput != Vector2.zero)
+        {
+            anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            anim.SetBool("isMoving", false);
+        }
+    }
+}
