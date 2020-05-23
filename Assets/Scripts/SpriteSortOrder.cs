d5b6775d5d1a99c1a102908fd80f130dfe2c76d8
Emil Föger <emil.foger@gmail.com>
2020-05-23 20:56:43 +0200

    Automatic Layer Sorting of Objects relative to their Y-Position

---
+++ Assets/Scripts/SpriteSortOrder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSortOrder : MonoBehaviour
+{
+    private SpriteRenderer theSR;       // Sprite Renderer Element
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // theSR als den SpriteRendere des jeweiligen Objekts setzen
+        theSR = GetComponent<SpriteRenderer>();
+
+        // Automatische Layersortierung der Objekte je nach Position auf de y-Achse
+        theSR.sortingOrder = Mathf.RoundToInt(transform.position.y * -10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
