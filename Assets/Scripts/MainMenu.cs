4bb533bc4b15836dd75d031a33cd3612029538ee
Emil Föger <emil.foger@gmail.com>
2020-05-31 18:09:02 +0200

    Added Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Variabeln Menu
+    public string levelToLoad;      // Welche Szene geladen werden soll
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Funktion Start Knopf
+    public void StartGame()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+
+    // Funktion Programm beenden
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
