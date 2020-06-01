53ba952ad026ab5b6945cdf6e357d10c8a6c464c
Emil Föger <emil.foger@gmail.com>
2020-06-01 14:18:51 +0200

    Updated Death Screen with Buttons

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
@@ -13,11 +14,15 @@ public class UIController : MonoBehaviour
     public Text healthText;             // REF Text des HP-Sliders
 
     // Variabeln Szenenübergang
-    public GameObject deathScreen;      // REF Death Screen
     public Image fadeScreen;            // REF Übergangsbild
     public float fadeSpeed;             // REF Übergangsgeschwindigkeit
     private bool fadeToBlack;           // REF ob Übergang zu Schwarz
     private bool fadeOutBlack;          // REF ob Übergang aus Schwarz
+
+    // Variabeln death screen
+    public GameObject deathScreen;      // REF Death Screen
+    public string newGameScene;         // REF Szene Nees Spiel
+    public string mainMenuScene;        // REF Szene Hauptmenü
 
 
     // Wie Start(), nur davor
@@ -82,5 +87,19 @@ public class UIController : MonoBehaviour
                 Mathf.MoveTowards
                 (fadeScreen.color.a, alpha, fadeSpeed * Time.deltaTime)
             );
+    }
+
+
+    // Funktion neues Spiel
+    public void NewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+
+
+    // Funktion zurück zum Hauptmenü
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(mainMenuScene);
     }
 }
