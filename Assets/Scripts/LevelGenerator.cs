cc2b32158d3403faf6c3403e7677af88dc1f4732
Emil Föger <emil.foger@gmail.com>
2020-06-03 01:08:35 +0200

    Automatic Outlines for rooms with one exit

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -16,11 +16,12 @@ public class LevelGenerator : MonoBehaviour
     public LayerMask whatIsRoom;                                            // REF Layer der iteriert werden soll
 
     // Variabeln Raumverfolgung
-    public Color startColor;                                                // REF Startfarbe
-    public Color endColor;                                                  // REF Endfarbe
+    public Color startColor;                                                // REF Startraumfarbe
+    public Color endColor;                                                  // REF Endraumfarbe
     private GameObject endRoom;                                             // REF Raumobjekt Levelende
-    private List<GameObject> layoutRoomObjects = new List<GameObject>();    // REF Liste aller Raumobjekte
-    public RoomPrefabs rooms;
+    private List<GameObject> layoutRoomObjects = new List<GameObject>();    // REF Liste aller generierten Raum-mockups
+    private List<GameObject> generatedOutlines = new List<GameObject>();    // REF Liste aller generierten Raumkontouren
+    public RoomPrefabs rooms;                                               // REF alle Raumkonturtypen
 
 
     // Start is called before the first frame update
@@ -62,6 +63,23 @@ public class LevelGenerator : MonoBehaviour
                 MoveGenerationPoint();
             }
         }
+
+        
+        //
+        // Raumkonturen generieren
+        //
+
+        // Startraum Generieren
+        CreateRoomOutline(Vector3.zero);
+
+        // Räume generieren
+        foreach (GameObject room in layoutRoomObjects)
+        {
+            CreateRoomOutline(room.transform.position);
+        }
+
+        // Endraum generieren
+        CreateRoomOutline(endRoom.transform.position);
     }
 
 
@@ -98,10 +116,55 @@ public class LevelGenerator : MonoBehaviour
                 break;
         }
     }
+
+
+    // Funktion Raumkonturen generieren
+    public void CreateRoomOutline(Vector3 roomPosition)
+    {
+        // Überprüfen ob andere Räume abgrenzen
+        bool roomAbove = Physics2D.OverlapCircle( roomPosition + new Vector3(0f, yOffset, 0f), 0.2f, whatIsRoom );
+        bool roomBelow = Physics2D.OverlapCircle( roomPosition + new Vector3(0f, -yOffset, 0f), 0.2f, whatIsRoom );
+        bool roomLeft = Physics2D.OverlapCircle( roomPosition + new Vector3(-xOffset, 0f, 0f), 0.2f, whatIsRoom );
+        bool roomRight = Physics2D.OverlapCircle( roomPosition + new Vector3(xOffset, 0f, 0f), 0.2f, whatIsRoom );
+
+        // Anzahl der Eingänge
+        int directionCount = 0;
+
+        // Eingänge zählen
+        if (roomAbove)  { directionCount++; }
+        if (roomBelow)  { directionCount++; }
+        if (roomLeft)   { directionCount++; }
+        if (roomRight)  { directionCount++; }
+        
+        //
+        switch (directionCount)
+        {
+            case 1:
+                if (roomAbove) { generatedOutlines.Add( Instantiate(rooms.singleUp, roomPosition, transform.rotation) ); }
+                if (roomBelow) { generatedOutlines.Add( Instantiate(rooms.singleDown, roomPosition, transform.rotation) ); }
+                if (roomLeft) { generatedOutlines.Add( Instantiate(rooms.singleLeft, roomPosition, transform.rotation) ); }
+                if (roomRight) { generatedOutlines.Add( Instantiate(rooms.singleRight, roomPosition, transform.rotation) ); }
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+        }
+  
+
+    }
 }
 
 
-
+// REF Klasse aller möglichen Raumeingangstypen
 [System.Serializable]
 public class RoomPrefabs
 {
