951a8566ff225a584dc1957ca46bef052f871a84
Emil Föger <emil.foger@gmail.com>
2020-06-17 00:38:36 +0200

    Added Room Center End with character cage

--- Assets/Scripts/CharacterUnlockCage.cs
+++ Assets/Scripts/CharacterUnlockCage.cs
@@ -5,24 +5,36 @@ using UnityEngine;
 public class CharacterUnlockCage : MonoBehaviour
 {
     // Variabeln Käfig entriegeln
-    private bool canUnlock;                     // Ob Käfig entriegelt werden kann
-    public GameObject message;                  // REF Nachricht Käfig
+    private bool canUnlock;                                 // Ob Käfig entriegelt werden kann
+    public GameObject message;                              // REF Nachricht Käfig
 
     // Variabeln Charakter auswählen
+    public CharacterSelector[] characterSelectors;          // REF Liste der möglichen Charaktere
+    private CharacterSelector playerToUnlock;               // Ausgewählter Charakter
+    public SpriteRenderer cagedSR;                          // REF SpriteRenderer des ausgewählten Charakters
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerToUnlock = characterSelectors[Random.Range(0, characterSelectors.Length)];
+        cagedSR.sprite = playerToUnlock.playerToSpawn.bodySR.sprite;
     }
+
 
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (canUnlock && Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(playerToUnlock, transform.position, transform.rotation);
+
+            gameObject.SetActive(false);
+        }
     }
+
 
 
     private void OnTriggerEnter2D(Collider2D other)
