018d170b6481c7254aecc1f337d16d929093037c
Emil Föger <emil.foger@gmail.com>
2020-06-16 23:46:00 +0200

    Added Cage gameobject and and unlock script

---
+++ Assets/Scripts/CharacterUnlockCage.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterUnlockCage : MonoBehaviour
+{
+    // Variabeln Käfig entriegeln
+    private bool canUnlock;                     // Ob Käfig entriegelt werden kann
+    public GameObject message;                  // REF Nachricht Käfig
+
+    // Variabeln Charakter auswählen
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canUnlock = true;
+            message.SetActive(true);
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            canUnlock = false;
+            message.SetActive(false);
+        }
+    }
+}
