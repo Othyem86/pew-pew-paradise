cd5572bae67f3bfc07f4cec1916e1ec5de85ad60
Emil Föger <emil.foger@gmail.com>
2020-06-18 23:52:09 +0200

    Added Delelte Saved Game functionality

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,7 +6,10 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Variabeln Menu
-    public string levelToLoad;      // REF Welche Szene geladen werden soll
+    public string levelToLoad;                          // REF Scene to load
+
+    public GameObject deletePanel;                      // REF Delete Saved Game Panel
+    public CharacterSelector[] charactersToDelete;      // REF Array of all unlockable Characters
 
 
     // Start is called before the first frame update
@@ -34,5 +37,30 @@ public class MainMenu : MonoBehaviour
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+
+    public void DeleteSave()
+    {
+        deletePanel.SetActive(true);
+    }
+
+
+
+    public void CancelDelete()
+    {
+        deletePanel.SetActive(false);
+    }
+
+
+
+    public void ConfirmDelete()
+    {
+        foreach(CharacterSelector theCharacter in charactersToDelete)
+        {
+            PlayerPrefs.SetInt(theCharacter.playerToSpawn.name, 0);
+        }
+
+        deletePanel.SetActive(false);
     }
 }
